eeef389a9b8d2a3849988e711f46d62d03a09ee6
Luiza Vavilova <yanushpant@gmail.com>
2026-05-22 17:03:41 +0300

    fix h/w

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,44 +2,59 @@ const formData = {
     email: "",
     message: ""
 }
-
 const key = "feedback-form-state";
 
-
 const form = document.querySelector(".feedback-form");
-const email = document.querySelector('input[type = "email"]');
+const email = document.querySelector('input[type="email"]');
 const comment = document.querySelector('textarea[name="message"]');
 
-const safeInfo = JSON.parse(localStorage.getItem(key));
-email.value = safeInfo.email || "";
-comment.value = safeInfo.message || "";
-
-
-form.addEventListener("input", addText);
+form.addEventListener("input", formInput);
 form.addEventListener("submit", formSubmit);
 
-function addText(event) {
+safeInfo(formData);
+email.value = formData.email;
+comment.value = formData.message;
+
+function formInput(event) {
     if (event.target.name === "email") {
-        formData.email = `${event.target.value}`;
-    } else { 
-        formData.message = `${event.target.value}`;
+        formData.email = event.target.value;  
+    } else if(event.target.name ==="message")  {
+        console.log(event.target.name);
+        formData.message = event.target.value;
     }
+
     localStorage.setItem(key, JSON.stringify(formData));
 }
-
 
 function formSubmit(event) {
     event.preventDefault();
+
     const elements = event.target.elements;
-    console.log(elements);
     if (elements.email.value === "" || elements.message.value === "") {
        return alert("Fill please all fields!");
+    } else {
+        formData.email = elements.email.value;
+        formData.message = elements.message.value;
+        localStorage.setItem(key, JSON.stringify(formData));
     }
-    formData.email = elements.email.value;
-    formData.message = elements.message.value;
-    localStorage.setItem(key, JSON.stringify(formData));
+    
     console.log(JSON.parse(localStorage.getItem(key)));
+
     event.target.reset();
+    localStorage.removeItem(key);
+    formData.email = "";
+    formData.message = "";
 }
 
+
+function safeInfo(obj) {
+    const localInfo = JSON.parse(localStorage.getItem(key));
+    if (!localInfo) {
+        obj.email = "";
+        obj.message = "";
+    } else {
+        obj.email = localInfo.email;
+        obj.message = localInfo.message;    
+    }
+}
 
